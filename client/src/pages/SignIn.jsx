9add78d64e1aeb7d5e74709d52fb24e05f71f5b8
Yash Agrahari <yashagrahari456@gmail.com>
2024-04-30 22:41:44 +0530

    Integrated Redux Toolkit, created store and userSlice, and dispatched actions in sign-in page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../app/user/userSlice.js'
 
 export default function SignUp() {
   const [ formData, setFormData ] = useState({});
-  const [ error, setError ] = useState(null);
-  const [ loading, setLoading ] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -15,7 +17,7 @@ export default function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true);
+      dispatchEvent(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -25,16 +27,13 @@ export default function SignUp() {
       });
       const data = await res.json();
       if(data.success === false){
-        setLoading(false);
-        setError(data.message);
+        dispatchEvent(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatchEvent(signInSuccess(data));
       navigate('/');
     } catch(error) {
-      setLoading(false);
-      setError(error.message);
+      dispatchEvent(signInFailure(error.message));
     }
   };
   return (
