e356d9cb1a2218953d53f7111dae442e921805e6
Yash Agrahari <yashagrahari456@gmail.com>
2024-06-11 01:35:58 +0530

    Finished create Listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,7 @@ import { updateUserStart, updateUserSuccess, updateUserFailure,
          signOutUserFailure,
          signOutUserSuccess
 } from "../app/user/userSlice";
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -139,6 +140,7 @@ export default function Profile() {
         <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' defaultValue={currentUser.email} onChange={handleChange} />
         <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>listing</Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer hover:underline' onClick={handleDeleteUser}>Delete Account</span>
